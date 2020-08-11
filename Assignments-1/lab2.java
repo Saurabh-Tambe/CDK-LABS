540f87e3add88e1fc372a4b8ecd49161a89ee4c1
Saurabh Tambe <43045107+Saurabh-Tambe@users.noreply.github.com>
2020-08-11 18:40:16 +0530

    Create lab2.java

---
+++ Assignments-1/lab2.java
@@ -0,0 +1,17 @@
+//PROBLEM STATEMENT:Read a double find its square root and display it.
+
+import java.util.Scanner;
+import java.lang.Math;
+public class lab2 {
+
+public static void main(String[] args) {
+		 
+double no;
+System.out.println("Enter no.");
+Scanner sc=new Scanner(System.in);
+no=sc.nextDouble();
+System.out.println("Square Root is:"+Math.sqrt(no));
+
+	}
+
+}
