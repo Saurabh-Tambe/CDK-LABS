a6a13a2c5fbc87235cc2b24302e3958cb526fa41
Saurabh Tambe <43045107+Saurabh-Tambe@users.noreply.github.com>
2020-08-11 18:39:02 +0530

    Create lab1.java

---
+++ Assignments-1/lab1.java
@@ -0,0 +1,8 @@
+//PROBLEM STATEMENTS:Display the message, ‘Lincoln said: "Four score and seven years ago"’.
+public class lab1 {
+
+	public static void main(String[] args) {
+ System.out.println("Lincoln said: \"Four score and seven years ago\"");
+	}
+
+}
